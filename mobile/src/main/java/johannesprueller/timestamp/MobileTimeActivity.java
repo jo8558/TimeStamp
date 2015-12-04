d2229c3cfed4037503b4ca12db7eba1b7cc75cb1
johannes <johannes.pru@gmail.com>
2015-12-04 17:25:26 +0100

    first setup

---
+++ mobile/src/main/java/johannesprueller/timestamp/MobileTimeActivity.java
@@ -0,0 +1,13 @@
+package johannesprueller.timestamp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MobileTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_time);
+    }
+}
