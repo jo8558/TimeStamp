a844be0290699295ec4525a79e2985ba4b6b4790
johannes <johannes.pru@gmail.com>
2015-12-17 03:19:28 +0100

    added Textclock and Chronometer

--- mobile/src/main/java/johannesprueller/timestamp/MobileTimeActivity.java
+++ mobile/src/main/java/johannesprueller/timestamp/MobileTimeActivity.java
@@ -9,11 +9,27 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Chronometer;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextClock;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewAnimator;
+
+import org.w3c.dom.Text;
+
+import java.util.Date;
+
+import static android.R.color.black;
+import static android.R.color.transparent;
 
 public class MobileTimeActivity extends AppCompatActivity {
 
@@ -25,17 +41,58 @@ public class MobileTimeActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_mobile_time);
 
-        NfcHelper helper = new NfcHelper(this, this);
+        mainLayout = (LinearLayout) this.findViewById(R.id.mainLayout);
+        timeInfo = (TextView)this.findViewById(R.id.timeInfo);
+        textClock = (TextClock)this.findViewById(R.id.textclock);
+
+        chronometer = (Chronometer)this.findViewById(R.id.timerview);
+
+//        if(textClock.is24HourModeEnabled())
+//        {
+//            textClock.setFormat24Hour(textClock.getFormat24Hour());
+//        }
+//        else
+//        {
+//            textClock.setFormat12Hour(textClock.getFormat12Hour());
+//        }
+
+
+
+        handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message message) {
+                if(message.what == 0) {
+                    String msg = message.obj.toString();
+                    Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        NfcHelper helper = new NfcHelper(this, handler);
         helper.InitializeNfc();
     }
 
-    public void onImageClick(View view)
-    {
-        ImageView imageView = (ImageView)view;
+    public void onCurrentTimeClick(View view) {
+        timeInfo.setText(R.string.workedTime);
+        textClock.setVisibility(View.GONE);
+        chronometer.setBase(SystemClock.elapsedRealtime());
+        chronometer.setVisibility(View.VISIBLE);
+        chronometer.start();
+    }
 
+    public void onWorkingTimeClick(View view){
+        String workedTime = chronometer.getText().toString();
+        chronometer.stop();
+        timeInfo.setText(R.string.currentTime);
+        chronometer.setVisibility(View.GONE);
+        textClock.setVisibility(View.VISIBLE);
     }
 
     private Context context;
+    private Handler handler;
 
-
+    private LinearLayout mainLayout;
+    private TextView timeInfo;
+    private TextClock textClock;
+    private Chronometer chronometer;
 }
--- mobile/src/main/java/johannesprueller/timestamp/NfcHelper.java
+++ mobile/src/main/java/johannesprueller/timestamp/NfcHelper.java
@@ -13,49 +13,34 @@ import android.os.Message;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
-/**
- * Created by Admin on 12.12.2015.
- */
 public class NfcHelper {
 
-    private final Context context;
+
     private Handler handler;
     private Activity activity;
 
-    public NfcHelper(final Context context, Activity activity) {
-        this.context = context;
+    public NfcHelper( Activity activity, Handler handler) {
         this.activity = activity;
+        this.handler = handler;
     }
 
     public void InitializeNfc() {
         boolean granted;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            granted = checkNfcPermission();
-        } else {
-            granted = true;
-        }
+        granted = Build.VERSION.SDK_INT < Build.VERSION_CODES.M || checkNfcPermission();
 
         if (granted) {
-            NfcManager manager = (NfcManager) context.getSystemService(Context.NFC_SERVICE);
+            NfcManager manager = (NfcManager) activity.getSystemService(Context.NFC_SERVICE);
             if (manager != null) {
                 NfcAdapter adapter = manager.getDefaultAdapter();
                 adapter.enableReaderMode(activity, new NfcCallcback(), NfcAdapter.FLAG_READER_NFC_A | NfcAdapter.FLAG_READER_NFC_B | NfcAdapter.FLAG_READER_NFC_F | NfcAdapter.FLAG_READER_NFC_V, null);
             }
         }
-
-        handler = new Handler(Looper.getMainLooper()) {
-            @Override
-            public void handleMessage(Message message) {
-                String msg = message.obj.toString();
-                Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
-            }
-        };
     }
 
 
     private boolean checkNfcPermission() {
         String permission = "android.permission.NFC";
-        int res = context.checkCallingOrSelfPermission(permission);
+        int res = activity.checkCallingOrSelfPermission(permission);
         return (res == PackageManager.PERMISSION_GRANTED);
     }
 
